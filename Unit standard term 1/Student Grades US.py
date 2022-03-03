b3f0822492c4666f90ca677c33afaba106b8e1f4
thorb <18296@lynfield.ac.nz>
2022-03-03 19:23:11 +1300

    import file function update: Displaying available files and opening selected txt or csv file

--- Unit standard term 1/Student Grades US.py
+++ Unit standard term 1/Student Grades US.py
@@ -1,16 +1,44 @@
+import os
 dictionary = {}
+# Yaeko Threlkeld
 
-def import_file(file):
+def import_file():
+    l = []
+    f = os.listdir('.')  # open folder
+    for i in range(len(f)):
+        if '.csv' in f[i]:
+            l.append(f[i])
+        elif '.txt' in f[i]:  # if file has extention txt or csv, display it
+            l.append(f[i])
+        else:
+            pass
+
+    print("files available:" + str(l))
+    while True:
+
+        file_select = int(input("please select files 1-" + str(len(l)) + "\t"))
+        if file_select in range(1, len(l) + 1, 1):
+            file = l[file_select - 1]  # starts at 0
+            break
+
     while True:
         with open(file) as file:
             for line in file:
-                key, info = line.split(",", 1)  # splits each line in doc into a key and info
+                id, name, info = line.split(",", 2)  # splits each line in doc into id, name, info
                 data = info.split(",")
-                dictionary[key] = data
+                dictionary[name] = data
 
             file.close()
             break
 
-import_file("student+data.csv")
-print(dictionary.get("2"))
-print(dictionary["2"])
+def main():
+    import_file()
+    while True:
+        student_name = input("What student are you looking up?\t")  # .lower() this
+        try:
+            print(dictionary.get(student_name))
+
+        except:
+            pass
+
+main()
